b25c7a01cf38d01bcc2ede9bf867cfa890a872ba
Tilungile <tilungile.s65@gmail.com>
2022-09-09 05:32:51 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/*
+ * main - entry point
+ *
+ * puts (Programming is like building a miltilingual puzzle)
+ * Return: 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
