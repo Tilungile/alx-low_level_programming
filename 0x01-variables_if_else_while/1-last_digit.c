073f61af2b53c6c23e750cbdda80a58dc5746022
Tilungile <tilungile.s65@gmail.com>
2022-09-12 15:21:00 -0700

    v

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ * Description: 'printing last dift of number'
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+	int n;
+	int a;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	a = n % 10;
+	if (a > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, a);
+	else if (a == 0)
+		printf("Last digit of %i is %i and is 0\n", n, a);
+	else
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, a);
+	return (0);
+}
