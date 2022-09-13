6f2eb367ca4f3f72b9193672a8c82f5369f04bec
Tilungile <tilungile.s65@gmail.com>
2022-09-13 02:31:48 -0700

    v

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * Desrciption: 'printing all alphabets in lowercase'
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	putchar('\n');
+	return (0);
+}
