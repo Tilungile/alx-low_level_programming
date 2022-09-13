5caa272dce477fc6d733884cc8b8eb0d5f542264
Tilungile <tilungile.s65@gmail.com>
2022-09-13 06:24:08 -0700

    v

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'doing numbers'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ten = 0;
+
+	while (ten <= 9)
+	{
+		printf("%i", ten);
+		ten++;
+	}
+	putchar('\n');
+	return (0);
+}
