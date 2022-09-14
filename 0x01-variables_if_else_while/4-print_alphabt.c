a2b8a041594672f838878e097a7335f3780d03a3
Tilungile <tilungile.s65@gmail.com>
2022-09-14 05:14:57 -0700

    v

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 /**
  * main - Entrypoint
+ * Description: 'except e and q'
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char alph = 'a';
 
-	while ((alph != 'e') && (alph != 'q') && (alph <= 'z'))
+	while (alph <= 'z')
 	{
-		putchar(alph);
+		if ((alph != 'e') && (alph != 'q'))
+		{
+			putchar(alph);
+		}
 		alph++;
 	}
 	putchar('\n');
